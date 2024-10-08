31f64c751f9fd8e9c67893cdca7fbd9215cabc3d
amritsagoo91 <amritsagoo91@gmail.com>
2024-10-08 16:40:04 -0700

    2.9* The Phonebook Step 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
 import { useState } from 'react'
 
+const Person = ({ person }) => <p>{person.name} {person.number}</p>
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number:'040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+  const [filterList, setFilterList] = useState(persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
   const handleSubmit = (e) => {
-    e.preventDefault();
-    const newPerson = { name: newName, number:newNumber }
-    const exist = persons.some(person=>person.name.toLowerCase().trim()===newName.toLowerCase().trim())
-    if(exist){
+    e.preventDefault()
+    const newPerson = { name: newName, number: newNumber }
+    const exist = persons.some(person => person.name.toLowerCase().trim() === newName.toLowerCase().trim())
+    if (exist) {
       alert(`${newName} is already added to phonebook`)
-    }else{
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    } else {
+      setPersons(persons.concat(newPerson))
+      setFilterList(filterList.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
     }
+  }
+
+  const handleFilter = (e) => {
+    const filterName = e.target.value
+    const filteredList_ = persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
+    setFilterList(filteredList_)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <input onChange={handleFilter} />
+      </div>
+      <h1>add new</h1>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={e => setNewName(e.target.value)} />
@@ -34,11 +52,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
+
       {
-        persons.map((person)=><li key={person.name}>{person.name} {person.number}</li>)
+        filterList.map((person) => <Person key={person.id} person={person} />)
       }
-      </ul>
+
     </div>
   )
 }
