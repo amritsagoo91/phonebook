4827a5cd03663b27970f36c72b2b2b4f0777a224
amritsagoo91 <amritsagoo91@gmail.com>
2024-10-16 13:35:04 -0700

    2.12: The Phonebook step 7

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+<<<<<<< HEAD
 import service from './service/numbers'
+=======
+import service from './services/numbers'
+>>>>>>> 4d6f9cf (2.12: The Phonebook step 7)
 
 const Person = ({ person }) => <p>{person.name} {person.number}</p>
 
@@ -10,19 +14,32 @@ const App = () => {
   const [persons, setPersons] = useState([])
 
   useEffect(() => {
+<<<<<<< HEAD
 
     service.getAll().then(intialData => {
       setPersons(intialData)
       setFilterList(intialData)
     })
 
+=======
+    service.getAll().then(response => {
+      setPersons(response.data)
+      setFilterList(response.data)
+    })
+>>>>>>> 4d6f9cf (2.12: The Phonebook step 7)
   }, [])
   const [filterList, setFilterList] = useState(persons)
 
   const addPerson = (newPerson) => {
+<<<<<<< HEAD
     service.create(newPerson).then(responseNew => {
       setPersons(persons.concat(responseNew))
       setFilterList(persons.concat(responseNew))
+=======
+    service.create(newPerson).then(response => {
+      setPersons(persons.concat(response.data))
+      setFilterList(persons.concat(response.data))
+>>>>>>> 4d6f9cf (2.12: The Phonebook step 7)
     }
     )
   }
