ec8ce637b873eb8fef1e586f22ed507780e42893
amritsagoo91 <amritsagoo91@gmail.com>
2024-10-16 13:48:46 -0700

    2.13 The Phonebook step 8

--- src/services/numbers.js
+++ src/services/numbers.js
@@ -2,11 +2,11 @@ import axios from "axios";
 const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
-    return axios.get(baseUrl)
+    return axios.get(baseUrl).then(response => response.data)
 }
 
 const create = (obj) => {
-    return axios.post(baseUrl, obj)
+    return axios.post(baseUrl, obj).then(response => response.data)
 }
 
 export default { getAll, create }
